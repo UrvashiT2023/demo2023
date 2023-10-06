f43486a923c28db14ba4a0617d290dd36f45c269
UrvashiTiwari <UrvashiTiwari@DESKTOP-4JRA4MB>
2023-10-06 15:57:39 +0530

    contorller commit

--- demo/src/main/java/com/Demo/demo/controller/EmployeeController.java
+++ demo/src/main/java/com/Demo/demo/controller/EmployeeController.java
@@ -1,7 +1,6 @@
 package com.Demo.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,10 +8,11 @@ import com.Demo.demo.services.EmployeeServices;
 
 @RestController
 public class EmployeeController {
-@Autowired
-private EmployeeServices empService;
-@RequestMapping("getName")
-public String getName() {
-	return empService.getName();
-}
+	@Autowired
+	private EmployeeServices empService;
+
+	@RequestMapping("getName")
+	public String getName() {
+		return empService.getName();
+	}
 }
